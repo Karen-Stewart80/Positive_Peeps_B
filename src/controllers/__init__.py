e694b2e6ec31819a8a4380d98ed40af7148e66ba
Karen-Stewart <kazstewart911@hotmail.com>
2021-02-25 14:03:08 +1000

    debug

--- src/controllers/__init__.py
+++ src/controllers/__init__.py
@@ -1,5 +1,5 @@
 from controllers.profiles_controller import profiles
-# from controllers.users_controller import auth
+from controllers.users_controller import auth
 from controllers.post_controller import posts
 
 
